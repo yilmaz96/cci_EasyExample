ff09afb12dc2268b0d3b3160ada6edcf9a993faf
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2020-09-13 14:55:22 +0200

    added ISO Designer Project.

---
+++ EasyExample/ISODesigner/MyWorkspace1/MyProject1/Output/MyProject1.iop.h
@@ -0,0 +1,39 @@
+// ISO-Designer ISO 11783   Version 5.5.0.4333 Jetter AG
+// Do not change!
+#define ISO_DESIGNATOR_WIDTH                 80
+#define ISO_DESIGNATOR_HEIGHT                80
+#define ISO_MASK_SIZE                       480
+#define ISO_VERSION_LABEL             "       "
+#define MASK_WIDTH                          480
+#define MASK_HEIGHT                         480
+#define WorkingSet_0                          0
+#define DataMask_Home                      1000
+#define Container_Gesamtzaehler            3000
+#define Container_Tageszaehler             3001
+#define SoftKeyMask_Home                   4000
+#define SoftKey_PlusPlus                   5000
+#define SoftKey_Reset_Gesamtzaehler        5001
+#define SoftKey_Reset_Tageszaehler         5002
+#define Button_PlusPlus                    6000
+#define Button_Reset_Gesamtzaehler         6001
+#define Button_Reset_Tageszaehler          6002
+#define OutputString_PlusPlus             11000
+#define OutputString_Fasszaehler          11001
+#define OutputString_Reset_Gesamtzaehler  11002
+#define OutputString_Gesamtzaehler        11003
+#define OutputString_Tageszaehler         11004
+#define OutputString_Reset_Tageszaehler   11005
+#define OutputNumber_Tageszaehler         12000
+#define OutputNumber_Gesamtzaehler        12003
+#define Meter_Gesamtzaehler               17000
+#define Meter_Tageszaehler                17001
+#define LinearBargraph_Gesamtzaehler      18000
+#define LinearBargraph_Tageszaehler       18001
+#define NumberVariable_Tageszaehler       21000
+#define NumberVariable_Gesamtzaehler      21001
+#define FontAttributes_Default            23001
+#define FontAttributes_Button             23002
+#define ObjectPointer_Null2               27000
+#define ObjectPointer_Null3               27001
+#define ObjectPointer_Null1               27002
+#define AuxFunction2_PlusPlus             29000
