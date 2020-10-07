d2c14edb5a9f51a3b803a6c6ee7d1c3a17c0722f
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2020-10-07 13:49:06 +0200

    ..

--- EasyExample/AppIso/App_VTClient.c
+++ EasyExample/AppIso/App_VTClient.c
@@ -452,7 +452,6 @@ static void CbAuxPrefAssignment(VT_AUXAPP_T asAuxAss[], iso_s16* ps16MaxNumberOf
    VT_AUXAPP_T asPrefAss[20];
 
    /* Reading stored preferred assignment */
-//   s16NumbOfPrefAssigns = IsoAuxReadAssignOfFile(asPrefAss);
    s16NumbOfPrefAssigns = getAuxAssignment("CF-A-AuxAssignment", asPrefAss);
 
    for (s16I = 0; s16I < s16NumbOfPrefAssigns; s16I++)
